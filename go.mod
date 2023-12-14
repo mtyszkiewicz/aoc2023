4dc24b6bcf601770ec20f40fe43a801d4c1aae59
Marcin Tyszkiewicz <56219944+mtyszkiewicz@users.noreply.github.com>
2023-12-14 16:05:34 +0100

    day 8

--- go.mod
+++ go.mod
@@ -3,12 +3,15 @@ module github.com/mtyszkiewicz/aoc2023
 go 1.21.4
 
 require (
-	github.com/alecthomas/participle/v2 v2.1.1 // indirect
-	github.com/deckarep/golang-set/v2 v2.5.0 // indirect
-	github.com/mihonen/polynomials v0.0.0-20231125161445-ab94cb01cf0f // indirect
+	github.com/alecthomas/participle/v2 v2.1.1
+	github.com/deckarep/golang-set/v2 v2.5.0
+	github.com/mihonen/polynomials v0.0.0-20231125161445-ab94cb01cf0f
+	github.com/schollz/progressbar/v3 v3.14.1
+)
+
+require (
 	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
 	github.com/rivo/uniseg v0.4.4 // indirect
-	github.com/schollz/progressbar/v3 v3.14.1 // indirect
 	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/term v0.15.0 // indirect
 	gonum.org/v1/gonum v0.14.0 // indirect
--- src/day081/main.go
+++ src/day081/main.go
@@ -6,37 +6,102 @@ import (
 	"os"
 
 	"github.com/alecthomas/participle/v2"
-	"github.com/alecthomas/participle/v2/lexer"
 )
 
-type Network map[string][]string
+type Node struct {
+	Left  string `parser:"'(' @Ident ','"`
+	Right string `parser:"@Ident ')'"`
+}
 
-func (net *Network) Capture(values []string) error {
-	if *net == nil {
-		*net = make(map[string][]string)
-	}
-	(*net)[values[0]] = []string{"xd"}
-	return nil
+type Location struct {
+	Current    string `parser:"@Ident"`
+	Directions *Node  `parser:"'=' @@"`
+}
+
+type Pouch struct {
+	Instructions string     `parser:"@Ident"`
+	Locations    []Location `parser:"@@*"`
 }
 
 type DesertMap struct {
-	Instructions string   `parser:"@Instr"`
-	Network      *Network `parser:"@Map*"`
+	Instructions   string
+	Network        map[string]Node
+	StartLocations []string
+}
+
+func (p *Pouch) GetDesertMap() *DesertMap {
+	dm := new(DesertMap)
+	dm.Instructions = p.Instructions
+	dm.Network = make(map[string]Node)
+	for _, loc := range p.Locations {
+		dm.Network[loc.Current] = *loc.Directions
+		if loc.Current[2] == 'A' {
+			dm.StartLocations = append(dm.StartLocations, loc.Current)
+		}
+	}
+	return dm
+}
+
+func (dm *DesertMap) FindCyclePeriod(current string) int {
+	stepCount := 0
+	for {
+		direction := dm.Instructions[stepCount%len(dm.Instructions)]
+
+		switch direction {
+		case 'L':
+			current = dm.Network[current].Left
+		case 'R':
+			current = dm.Network[current].Right
+		}
+		stepCount++
+		if current[2] == 'Z' {
+			return stepCount
+		}
+	}
+}
+
+func gcd(a, b int) int {
+	// Euclidean algorithm
+	for b != 0 {
+		var temp = b
+		b = a % b
+		a = temp
+	}
+	return a
+}
+
+func lcm(a, b int) int {
+	return (a * b / gcd(a, b))
+}
+
+func lcmm(args []int) int {
+	// Recursively iterate through pairs of arguments
+	// i.e. lcm(args[0], lcm(args[1], lcm(args[2], args[3])))
+
+	if len(args) == 2 {
+		return lcm(args[0], args[1])
+	} else {
+		var arg0 = args[0]
+		args = args[1:]
+		return lcm(arg0, lcmm(args))
+	}
 }
 
 func main() {
-	lexer := lexer.MustSimple([]lexer.SimpleRule{
-		{"Instr", `[A-Z]+\n`},
-		{"Whitespace", `[ \t\r\n]+`},
-		{"Map", `([A-Z]+) = \(([A-Z]+), ([A-Z]+)\)`},
-	})
-	parser := participle.MustBuild[DesertMap](
-		participle.Lexer(lexer),
-		participle.Elide("Whitespace"),
-	)
-	desertMap, err := parser.Parse("", os.Stdin)
+	parser := participle.MustBuild[Pouch]()
+	pouch, err := parser.Parse("", os.Stdin)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(desertMap.Network)
+	desertMap := pouch.GetDesertMap()
+
+	periods := []int{}
+	for _, current := range desertMap.StartLocations {
+		period := desertMap.FindCyclePeriod(current)
+		periods = append(periods, period)
+		if current == "AAA" {
+			fmt.Printf("Part 1: %d\n", period)
+		}
+	}
+	fmt.Printf("Part 2: %d\n", lcmm(periods))
 }
